d728f007b4f4d6f16d7349103754bef50a1a4bcc
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-10-13 18:40:01 -0300

    Created controllers for the views

--- GladiaSystem/Controllers/HomeController.cs
+++ GladiaSystem/Controllers/HomeController.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
 
 namespace GladiaSystem.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Adm()
+        {
+            return View();
+        }
+        public ActionResult Agenda()
+        {
+            return View();
+        }
+        public ActionResult POS()
+        {
+            return View();
+        }
+        public ActionResult Employee()
+        {
+            return View();
+        }
+        public ActionResult Pet()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
         {
             return View();
         }
