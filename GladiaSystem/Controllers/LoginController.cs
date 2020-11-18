5063376043da5caff97b64ed4919fa93a8bf7707
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-11-18 14:05:00 -0300

    Login

---
+++ GladiaSystem/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using GladiaSystem.Database;
+using GladiaSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace GladiaSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        Queries queries = new Queries();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult CheckUser(User u)
+        {
+            queries.TestUser(u);
+            return Redirect("");
+        }
+    }
+}
